0e7acb7e543ff24ddeaed1c1fe07caa4a0bbbac4
kalphageek <kalphageek@outlook.com>
2020-09-05 13:42:33 +0900

    first build

---
+++ kafka-streams-quickstart/src/main/java/me/kalpha/kafka/streams/KafkaStreamsQuickstartApplication.java
@@ -0,0 +1,13 @@
+package me.kalpha.kafka.streams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamsQuickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamsQuickstartApplication.class, args);
+	}
+
+}
---
+++ kafka-streams-quickstart/src/test/java/me/kalpha/kafka/streams/KafkaStreamsQuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kalpha.kafka.streams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsQuickstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
